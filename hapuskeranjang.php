e6d57217268b624e6d315ee25905d94fc677d59a
Andreas Nugroho <79098402+AndreasNugoho@users.noreply.github.com>
2021-11-30 19:39:21 +0700

    Delete hapuskeranjang.php

--- hapuskeranjang.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$id_barang = $_GET['id'];
-unset($_SESSION['keranjang'][$id_barang]);
-echo "<script>alert('barang berhasil di hapus');</script>";
-echo "<script>location='cart.php';</script>";
-?>
